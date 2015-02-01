6c505fbe7a26cef6d2caf0b0591632dff249f7b2
user <user@DIABLO2>
2015-02-01 11:47:46 -0500

    Added lifter and grabber

---
+++ src/Grabber.java
@@ -0,0 +1,38 @@
+
+import edu.wpi.first.wpilibj.Solenoid;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author user
+ */
+public class Grabber {
+
+    private final Solenoid left1, left2, right1, right2;
+
+    public Grabber(Solenoid left1, Solenoid left2, Solenoid right1, Solenoid right2) {
+        this.left1 = left1;
+        this.left2 = left2;
+        this.right1 = right1;
+        this.right2 = right2;
+    }
+
+    public void grab() { //Extends all pistons to grab crate
+        left1.set(true);
+        left2.set(true);
+        right1.set(true);
+        right2.set(true);
+    }
+
+    public void release() {//Retracts all pistons to release crate
+        left1.set(false);
+        left2.set(false);
+        right1.set(false);
+        right2.set(false);
+    }
+
+}
---
+++ src/Lifter.java
@@ -0,0 +1,58 @@
+
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.Solenoid;
+import edu.wpi.first.wpilibj.Talon;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author user
+ */
+public class Lifter {
+    private final int speed = 1;
+    private final DigitalInput switchBot, switchMid, switchTop;
+    private final Talon lift1, lift2;
+
+    public Lifter(Talon lift1, Talon lift2, DigitalInput switchBot, DigitalInput switchMid, DigitalInput switchTop) {
+        this.lift1 = lift1;
+        this.lift2 = lift2;
+        this.switchBot = switchBot;
+        this.switchMid = switchMid;
+        this.switchTop = switchTop;
+    }
+    
+    public void bottom(){// Goes down until reaches bottom switch
+        lift1.set(speed);
+        lift2.set(-speed);
+        if(switchBot.get()){
+            lift1.set(0);
+            lift2.set(0);
+        }
+    }
+    public void middle(){
+        if(switchBot.get()){ // If at bottom, goes up 
+            lift1.set(-speed);
+            lift2.set(speed);          
+        }else{                //If at top, goes down
+            lift1.set(speed);
+            lift2.set(-speed);            
+        }
+        if(switchMid.get()){ // Stops at middle switch
+                lift1.set(0);
+                lift2.set(0);
+            }
+    }
+    public void top(){ // Goes up until reaces top switch
+        lift1.set(-speed);
+        lift2.set(speed);
+        if(switchTop.get()){
+            lift1.set(0);
+            lift2.set(0);
+        }
+    }
+
+}
