50d1e9f7fe7edcf5c10a7322159b920bedf18748
Erich Maas <knotsofunny@gmail.com>
2015-02-15 16:26:18 -0500

    Added logic to the Roller class

--- src/diablo2015/Roller.java
+++ src/diablo2015/Roller.java
@@ -25,11 +25,12 @@ import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.Relay;
 
 /**
- *Controls the rollers that pull the totes in
- * 
+ * Controls the rollers that pull the totes in
+ *
  * @author Erich Maas
  */
 public class Roller implements Tickable {
+
     /**
      * Counter for the tick method.
      */
@@ -47,51 +48,60 @@ public class Roller implements Tickable {
      */
     private final DigitalInput toteSwitch;
     /**
-     * Sets if the rollers will go in, out, or be stopped 
+     * Sets if the rollers will go in, out, or be stopped
      */
     private int speed = 0;
-/**
- * Constructs the roller object
- * @param left Spike for the left motor
- * @param right Spike for the right motor
- */  
+
+    /**
+     * Constructs the roller object
+     *
+     * @param left Spike for the left motor
+     * @param right Spike for the right motor
+     */
     public Roller(Relay left, Relay right, DigitalInput toteSwitch) {
         this.left = left;
         this.right = right;
         this.toteSwitch = toteSwitch;
     }
-/**
- * Both Rollers are set to pull in at max speed.
- * Motors spin in opposite directions
- */
+
+    /**
+     * Both Rollers are set to pull in at max speed. Motors spin in opposite
+     * directions
+     */
     public void in() { //Both rollers go in max speed
         speed = 1;
     }
-/**
- * Both Rollers are set to push out at maximum speed.
- * Motors spin in opposite directions.
- */
+
+    /**
+     * Both Rollers are set to push out at maximum speed. Motors spin in
+     * opposite directions.
+     */
     public void out() {//Both rollers go out max speed
         speed = -1;
     }
-/**
- * Controls direction of the rollers and time the rollers are activated.
- */
+
+    /**
+     * Controls direction of the rollers and time the rollers are activated.
+     */
     public void tick() {
-        
-        if(speed < 0 && !toteSwitch.get() && tickCount < 31){
+
+        if (speed < 0 && !toteSwitch.get()) {
             left.set(Relay.Value.kOn);
             right.set(Relay.Value.kReverse);
-            tickCount++;
-        }
-        if(speed > 0){
+        } else if (speed > 0) {
             left.set(Relay.Value.kReverse);
             right.set(Relay.Value.kOn);
+        } else {
+           left.set(Relay.Value.kOff);
+           right.set(Relay.Value.kOff);
         }
-        else{
+        if (tickCount > 31 || toteSwitch.get()) {
             speed = 0;
+            tickCount = 0;
             left.set(Relay.Value.kOff);
             right.set(Relay.Value.kOff);
+        } else {
+            tickCount++;
         }
     }
 
